48d7670c37a2d71a0497b2fd6942d857a27a4394
Elie Rotenberg <elie@rotenberg.io>
2020-10-06 20:01:17 +0200

    Full example

---
+++ diagram.py
@@ -0,0 +1,36 @@
+from diagrams import Cluster, Diagram
+from diagrams.onprem.analytics import Spark
+from diagrams.onprem.compute import Server
+from diagrams.onprem.database import PostgreSQL
+from diagrams.onprem.inmemory import Redis
+from diagrams.onprem.logging import Fluentd
+from diagrams.onprem.monitoring import Grafana, Prometheus
+from diagrams.onprem.network import Nginx
+from diagrams.onprem.queue import Kafka
+
+with Diagram("Advanced Web Service with On-Premise", show=False):
+    ingress = Nginx("ingress")
+
+    metrics = Prometheus("metric")
+    metrics << Grafana("monitoring")
+
+    with Cluster("Service Cluster"):
+        grpcsvc = [
+            Server("grpc1"),
+            Server("grpc2"),
+            Server("grpc3")]
+
+    with Cluster("Sessions HA"):
+        master = Redis("session")
+        master - Redis("replica") << metrics
+        grpcsvc >> master
+
+    with Cluster("Database HA"):
+        master = PostgreSQL("users")
+        master - PostgreSQL("slave") << metrics
+        grpcsvc >> master
+
+    aggregator = Fluentd("logging")
+    aggregator >> Kafka("stream") >> Spark("analytics")
+
+    ingress >> grpcsvc >> aggregator
